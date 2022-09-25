ea0821a259aecd06fe52598ea5d20cd9c01c6db7
maojx123 <122036482@qq.com>
2022-09-25 23:55:46 +0800

    init_blog_project

---
+++ .vitepress/config.js
@@ -0,0 +1,35 @@
+const nav = [
+  {
+    text: "工作",
+    items: [
+      { text: "Item A", link: "/item-1" },
+      { text: "Item B", link: "/item-2" },
+      { text: "Item C", link: "/item-3" },
+    ],
+  },
+  {
+    text: "书籍",
+    items: [
+      { text: "javascript忍者秘籍", link: "/item-1" },
+      { text: "javassript数据结构与算法", link: "/item-2" },
+      { text: "javascript高级程序设计", link: "/item-3" },
+      { text: "javascript悟道", link: "/item-3" },
+    ],
+  },
+  { text: "面试", link: "" },
+];
+
+export default {
+  // themeConfig: {
+  //   siteTitle: "MJX_Blog",
+  // },
+  title: "MJX的博客",
+  description: "记录生活，工作，与学习",
+  srcDir: "src",
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    nav,
+  },
+};
---
+++ src/workutils/common.md
@@ -0,0 +1,26 @@
+# 工作中常用的工具函数
+## 防抖函数
+``` js
+   function debounce(fn,delay = 200) {
+    let timer= null;
+    return function() {
+        if (timer) clearSetTimeout(timer)
+        timer = setTimeout(() =>{
+            fn.apply(this, arguments);
+        },delay);
+    }
+   }
+```
+## 节流函数
+``` js
+   function throttle(fn,delay = 200) {
+    let timer= null;
+    return function() {
+        if (timer) return
+        timer = setTimeout(() =>{
+            fn.apply(this, arguments);
+            timer = null
+        },delay);
+    }
+   }
+```
